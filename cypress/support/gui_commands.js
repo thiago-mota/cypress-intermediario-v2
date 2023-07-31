e4e7383969995b99332323a711c473b17720397d
Thiago Mota <th.araujomota@gmail.com>
2023-07-31 03:25:37 -0300

    remove useless lines

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -59,8 +59,6 @@ Cypress.Commands.add('gui_setLabelOnInssue', (label) => {
 })
 
 Cypress.Commands.add('gui_setMilestoneOnIssue', (milestone) => {
-  cy.get('[data-qa-selector="milestone_block"] .gl-button-text').click()
-  cy.contains(milestone.title).click()
-  cy.get('[data-testid="issue-title"]').click();
-  cy.contains(milestone.title);
+  cy.get('[data-qa-selector="milestone_block"] .gl-button-text').click();
+  cy.contains(milestone.title).click();
 });
